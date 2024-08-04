f3cafadef484b2450aa0ac35702d286ebb8754c8
EC2 Default User <ec2-user@ip-172-31-19-167.ap-northeast-1.compute.internal>
2024-08-04 13:47:41 +0000

    [Add] コードに対するコメントの追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,12 +1,13 @@
 class RelationshipsController < ApplicationController
 
+  #アクセスしようとしてるユーザーがログイン済みかどうかチェック
   before_action :authenticate_user!
 
   def create
     #フォロー対象のユーザーを特定
     user = User.find(params[:user_id])
     #ログインしてるユーザーがその↑ユーザーをフォロー
-    current_user.follow(user)
+    current_user.follow(user)   #models/user.rbで定義したfollow(user)メソッド呼び出し
     redirect_to request.referer
   end
 
@@ -14,7 +15,7 @@ class RelationshipsController < ApplicationController
     #フォロー解除対象のユーザーを特定
     user = User.find(params[:user_id])
     #ログインしてるユーザーがその↑ユーザーのフォローを解除
-    current_user.unfollow(user)
+    current_user.unfollow(user)   #models/user.rbで定義したunfollow(user)メソッド
     redirect_to request.referer
   end
 
