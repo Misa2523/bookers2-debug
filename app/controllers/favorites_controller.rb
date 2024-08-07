cd504a2cb10c6dd7c867ea61b4b5a03de7380515
EC2 Default User <ec2-user@ip-172-31-19-167.ap-northeast-1.compute.internal>
2024-08-07 15:10:07 +0000

    [Add] いいねの非同期化を実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,18 @@ class FavoritesController < ApplicationController
     #URL末尾のidを持つ書籍の情報をbookに格納
     book = Book.find(params[:book_id])
     #ログインしてるユーザー情報とbook_comment_paramsで指定されたコメント情報を新規作成し、favoriteに格納
-    favorite = current_user.favorites.new(book_id: book.id)
+    @favorite = current_user.favorites.new(book_id: book.id)
     # ↑ book_idというキーにbook.idという値を関連付けている（作成された`favorite`オブジェクトの属性`book_id`の値が、`book`オブジェクトの`id`と同じになる）
     # ↑ ハッシュと呼ばれるデータ構造
 
-    favorite.save
+    @favorite.save
+
+    #上記の変数において、jsファイルにコントローラから情報を渡すには、ビューに情報を渡す時と同じように@を付けてインスタンス変数にする
+    #飛ばすjsファイルを記述
+    render 'replace_btn'
 
     #同じページに遷移（いいねはindexページとshowページ両方で行うためこれを使う）
-    redirect_to  request.referer    #リファラー
+    #redirect_to  request.referer    #リファラー
   end
 
   def destroy
@@ -20,11 +24,15 @@ class FavoritesController < ApplicationController
 
     #ログインしてるユーザーが特定の本(book)をいいねしているか調べる
     #current_userのいいね(favortes)から、book_idが指定された本を検索し、見つかればfavoriteに代入
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @favorite = current_user.favorites.find_by(book_id: book.id)
     #モデル名.find_by(条件)　検索でヒットしたレコードの最初１件のみを返すメソッド
 
-    favorite.destroy
-    redirect_to  request.referer
+    @favorite.destroy
+
+    #飛ばすjsファイルを記述
+    render 'replace_btn'
+
+    #redirect_to  request.referer
   end
 
 end
