da71a133b72e3a205a44b91f78bea50800169fc8
EC2 Default User <ec2-user@ip-172-31-19-167.ap-northeast-1.compute.internal>
2024-08-16 03:21:38 +0000

    [Update] アクセス権限の変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "bee7582d70664d4dabb296e70ab18cc5.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  #誰でもアクセスできるよう記述変更
+  config.hosts.clear
 end
